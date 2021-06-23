c077538a4eaeca3ab5f53ad9a0754fa2affddeae
Jessica Yao Long <jessica.y.long@gmail.com>
2021-06-23 15:53:07 -0400

    Added functions

---
+++ R/graph_hmm.R
@@ -0,0 +1,12 @@
+graph_hmm_output <- function(output) {
+  ggplot(output, aes(x = index, y = obs, color = state)) +
+    geom_line() +
+    theme_minimal() +
+    scale_colour_continuous(type = "viridis")
+}
+
+graph_hmm_hist <- function(output) {
+  ggplot(output, aes(obs)) +
+    geom_histogram(binwidth = 1, colour = "navy", fill = "light blue") +
+    theme_minimal()
+}
---
+++ R/misc.R
@@ -0,0 +1,43 @@
+#' @import bayesforecast
+#' @import chron
+#' @import dplyr
+#' @import GGally
+#' @import ggfortify
+#' @import ggplot2
+#' @importFrom gridExtra grid.arrange
+#' @importFrom lemon grid_arrange_shared_legend
+#' @import mvtnorm
+#' @import plyr
+#' @import RColorBrewer
+#' @import stats
+#' @import tidyr
+#' @import utils
+
+diag_exp <- function(mat) {
+  diag(mat) <- exp(diag(mat))
+  return(mat)
+}
+
+diag_log_lower <- function(mat) {
+  diag(mat) <- log(diag(mat))
+  vect <- mat[lower.tri(mat, diag = TRUE)]
+  return(vect)
+}
+
+mvnorm_densities <- function(x, mod, m) {
+  pvect <- numeric(m)
+  for (i in 1:m) {
+    pvect[i] <- dmvnorm(x, mod$mu[[i]], mod$sigma[[i]])
+  }
+  return(pvect)
+}
+
+mvnorm_hmm_sample_one <- function(state, mod) {
+  x <- rmvnorm(1, mean = mod$mu[[state]], sigma = mod$sigma[[state]])
+  return(x)
+}
+
+triangular_num <- function(n) {
+  nums <- choose(seq(n + 1), 2)
+  return(nums[n + 1])
+}

39507e9aea033c51e59cc2cd8e54a177180c83f5
longjess <86081269+longjess@users.noreply.github.com>
2021-07-13 17:15:50 -0400

    Update graph_hmm.R

--- R/graph_hmm.R
+++ R/graph_hmm.R
@@ -25,7 +25,31 @@ graph_hmm_output <- function(output) {
 #'
 #' @examples
 graph_hmm_hist <- function(output) {
-  ggplot(output, aes(obs)) +
+  plot <- ggplot(output, aes(obs)) +
     geom_histogram(binwidth = 1, colour = "navy", fill = "light blue") +
     theme_minimal()
+  return(plot)
+}
+
+pseudo_residual_plot <- function(data) {
+  # Index plot of pseudo-residuals
+  plot_index <- ggplot(data) +
+    geom_point(aes(x = index, y = npsr), size = 0.5, colour = "black") +
+    theme_minimal()
+  # Histogram of pseudo-residuals
+  plot_hist <- ggplot(data, aes(npsr)) +
+    geom_histogram(aes(y = ..density..), colour = "navy", fill = "light blue") +
+    stat_function(fun = dnorm, colour = "red") +
+    theme_minimal()
+  # QQ plot of pseudo-residuals
+  plot_qq <- ggplot(data, aes(sample = npsr)) +
+    stat_qq() +
+    stat_qq_line() +
+    theme_minimal()
+  # ACF of pseudo-residuals
+  plot_acf <- ggacf(data$npsr) +
+    theme_minimal()
+  plot <- grid.arrange(plot_index, plot_hist, plot_qq, plot_acf,
+                       nrow = 2, ncol = 2)
+  return(plot)
 }
